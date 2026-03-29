5fb3f1abf2995554ca45c2007462f92264e065e8
Ali Raza <syedalirazaabeditechsunset@gmail.com>
2026-03-29 15:21:14 +0530

    Changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
-import "./App.css"
+import "./App.css";
 
 function App() {
   return (
-    <main>
-      <div className="container">
-        <div className="box box1">
-          <button className="styleButton">Click to buy</button>
+    <>
+      <nav className="navbar">
+        <h2 className="logo">MyShop</h2>
+        <ul className="navLinks">
+          <li>Home</li>
+          <li>Products</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <button className="loginBtn">Login</button>
+      </nav>
+      <main>
+        <div className="container">
+          <div className="box box1">
+            <button className="styleButton">Click to buy</button>
+          </div>
+          <div className="box box2">
+            <button className="styleButton">Click to buy</button>
+          </div>
+          <div className="box box3">
+            <button className="styleButton">Click to buy</button>
+          </div>
+          <div className="box box4">
+            <button className="styleButton">Click to buy</button>
+          </div>
+          <div className="box box5">
+            <button className="styleButton">Click to buy</button>
+          </div>
+          <div className="box box6">
+            <button className="styleButton">Click to buy</button>
+          </div>
         </div>
-        <div className="box box2">
-          <button className="styleButton">Click to buy</button>
-        </div>
-        <div className="box box3">
-          <button className="styleButton">Click to buy</button>
-        </div>
-        <div className="box box4">
-          <button className="styleButton">Click to buy</button>
-        </div>
-        <div className="box box5">
-          <button className="styleButton">Click to buy</button>
-        </div>
-        <div className="box box6">
-          <button className="styleButton">Click to buy</button>
-        </div>
-      </div>
-    </main>
-  )
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
